44c387235b772164fd686c716e532624e3a4983f
aonemd <aonemdsaleh@gmail.com>
2018-06-21 21:57:15 +0200

    Add `Geckorate::Decorator.decorate_collection`

--- lib/geckorate.rb
+++ lib/geckorate.rb
@@ -3,5 +3,16 @@ require "geckorate/version"
 module Geckorate
   class Decorator < SimpleDelegator
     def decorate; end
+
+    class << self
+      def decorate_collection(collection)
+        collection.map do |item|
+          klass           = item.class
+          full_klass_name = klass.to_s.concat('Decorator')
+          decorator_klass = Class.const_get(full_klass_name)
+          decorator_klass.new(item).decorate
+        end
+      end
+    end
   end
 end
