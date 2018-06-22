97529e636708a1a92fd647e8acff91d1b06c1cb0
aonemd <aonemdsaleh@gmail.com>
2018-06-22 19:25:51 +0200

    Add `decorate_kaminari_collection`

--- lib/geckorate.rb
+++ lib/geckorate.rb
@@ -13,6 +13,15 @@ module Geckorate
           decorator_klass.new(item).decorate
         end
       end
+
+      def decorate_kaminari_collection(collection)
+        {
+          page: collection.current_page,
+          per_page: collection.default_per_page,
+          total: collection.total_count,
+          records: decorate_collection(collection)
+        }
+      end
     end
   end
 end
