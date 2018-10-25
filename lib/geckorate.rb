c39c7eee39dcf83d7e430012bfd7f726d7f05bf3
aonemd <aonemdsaleh@gmail.com>
2018-10-25 16:02:51 +0200

    Add an optional `class_name` to `decorate_collection`
    This makes it possible to manually set the decorator class name instead
    of automatically inferring it.
    
    One use case for this is when the model class name is different from the
    decorator class name.

--- lib/geckorate.rb
+++ lib/geckorate.rb
@@ -5,10 +5,10 @@ module Geckorate
     def decorate; end
 
     class << self
-      def decorate_collection(collection)
+      def decorate_collection(collection, class_name = nil)
         return [] if collection.empty?
 
-        klass           = collection.first.class
+        klass           = class_name || collection.first.class
         full_klass_name = klass.to_s.concat('Decorator')
         decorator_klass = Class.const_get(full_klass_name)
 
@@ -17,21 +17,21 @@ module Geckorate
         end
       end
 
-      def decorate_kaminari_collection(collection)
+      def decorate_kaminari_collection(collection, class_name = nil)
         {
           page: collection.current_page,
           per_page: collection.current_per_page,
           total: collection.total_count,
-          records: decorate_collection(collection)
+          records: decorate_collection(collection, class_name)
         }
       end
 
-      def decorate_will_paginate_collection(collection)
+      def decorate_will_paginate_collection(collection, class_name = nil)
         {
           page: collection.current_page,
           per_page: collection.per_page,
           total: collection.total_entries,
-          records: decorate_collection(collection)
+          records: decorate_collection(collection, class_name)
         }
       end
     end
