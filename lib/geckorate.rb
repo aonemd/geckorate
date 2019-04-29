51976e48ef8bd8b6181731747ef0fe9748d58610
aonemd <aonemdsaleh@gmail.com>
2019-04-29 14:37:15 +0200

    Update function definitions to use keywork args

--- lib/geckorate.rb
+++ lib/geckorate.rb
@@ -5,7 +5,7 @@ module Geckorate
     def decorate; end
 
     class << self
-      def decorate_collection(collection, class_name = nil)
+      def decorate_collection(collection, class_name: nil)
         return [] if collection.empty?
 
         klass           = class_name || collection.first.class
@@ -17,21 +17,21 @@ module Geckorate
         end
       end
 
-      def decorate_kaminari_collection(collection, class_name = nil)
+      def decorate_kaminari_collection(collection, class_name: nil)
         {
           page: collection.current_page,
           per_page: collection.current_per_page,
           total: collection.total_count,
-          records: decorate_collection(collection, class_name)
+          records: decorate_collection(collection, class_name: class_name)
         }
       end
 
-      def decorate_will_paginate_collection(collection, class_name = nil)
+      def decorate_will_paginate_collection(collection, class_name: nil)
         {
           page: collection.current_page,
           per_page: collection.per_page,
           total: collection.total_entries,
-          records: decorate_collection(collection, class_name)
+          records: decorate_collection(collection, class_name: class_name)
         }
       end
     end
